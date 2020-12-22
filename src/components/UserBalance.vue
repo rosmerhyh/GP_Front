d81e7e5c2349943b329f36d41a9924276e6713f3
rosmerhyh <rosmerhyh@gmail.com>
2020-12-21 22:50:57 -0500

    rename typo
    co-authored-by: @rosmerh

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,7 +20,7 @@
           this.username = this.$route.params.username
 
           let self = this
-          axios.get("https://cajero-api.herokuapp.com/user/balance/" + this.username)
+          axios.get("https://gestor-proyectosk.herokuapp.com/user/balance/" + this.username)
               .then((result) => {
                   self.balance = result.data.balance
               })
